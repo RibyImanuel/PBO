c75c75f86e9970d8588f014a6fbb6e7feecd5a79
RibyImanuel <114047835+RibyImanuel@users.noreply.github.com>
2024-03-13 18:39:49 +0700

    Add files via upload

---
+++ PB_Objek/week1/volume_tabung.java
@@ -0,0 +1,16 @@
+package week1;
+
+public class volume_tabung {
+
+	public static void main(String[] args) {
+		double diameter = 5;
+        double tinggi = 10;
+
+        double volume = Math.PI * Math.pow(diameter / 2, 2) * tinggi;
+
+        System.out.println("Volume tabung: " + volume);
+        
+
+	}
+
+}
